cd90f107b64ea7551a979e711e8aec7be8cd0c69
Jokeren <robinho364@gmail.com>
2024-08-07 20:40:15 -0400

    Update

--- include/triton/Tools/LinearLayout.h
+++ include/triton/Tools/LinearLayout.h
@@ -594,9 +594,9 @@ public:
   LinearLayout sublayout(ArrayRef<StringAttr> inDimNames,
                          ArrayRef<StringAttr> outDimNames) const;
 
-  // Is there any value in the basis vector that is zero?
-  bool sublayoutHasZero(ArrayRef<StringAttr> inDimNames,
-                        ArrayRef<StringAttr> outDimNames) const;
+  // Is the sublayout restricted to inDimNames + outDimNames all zeros?
+  bool sublayoutIsZero(ArrayRef<StringAttr> inDimNames,
+                       ArrayRef<StringAttr> outDimNames) const;
 
   // Is the sublayout restricted to inDimNames + outDimNames and then flattened
   // to 1D the identity layout (ignoring out-dim sizes)?
